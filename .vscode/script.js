8775bc78b39296c1f80394f842a598b534ba4115
Thullio Ferreira <thullioferreira@hotmail.com>
2023-03-10 15:52:27 -0300

    projeto trote

---
+++ .vscode/script.js
@@ -0,0 +1,23 @@
+function toggleMOde() {
+  const html= document.documentElement
+  html.classList.toggle("light")
+
+  //if(html.classList.contains('light')){
+  //html.classList.remove('light')
+  //} else{
+  //html.classList.add('light')
+  //}
+
+  const img= document.querySelector("#profile img")
+  if(html.classList.contains("light")){
+  img.setAttribute("src","./assets/avatar-light.png")
+  } else{
+  img.setAttribute("src", "./assets/avatar.png")
+  }
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("alt", "logo trote solidário 2")
+  } else {
+    img.setAttribute("alt", "logo trote solidário")
+  }
+}  
