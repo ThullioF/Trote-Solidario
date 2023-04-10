6b54ee431780bc2164e65b523416ee2e76974a23
Thullio Ferreira <thullioferreira@hotmail.com>
2023-04-10 17:16:52 -0300

    calculadora

---
+++ kits.js
@@ -0,0 +1,30 @@
+function calcularKits(){
+    // recupera a qtde de kits
+    let kits = Number(document.getElementById("kits").value)
+    // calcula qts pontos a equipe conseguiu
+    let pontos
+    if (kits >= 80){
+        pontos = 5000
+    }
+    else if ((kits >= 64) && (kits < 80)){
+        pontos = 4000
+    }
+    else if ((kits >= 40) && (kits < 64)){
+        pontos = 2500
+    }
+    else if ((kits >= 16) && (kits < 40)){
+        pontos = 1000
+    }
+    else if ((kits >=0) && kits < 16){
+        pontos = 0
+    }
+    else {
+        pontos = -1 
+    }
+    if (pontos == -1){
+        document.getElementById("pontos").innerHTML = "Impossível calcular"
+    }
+    else {
+        document.getElementById("pontos").innerHTML = "A pontuação foi de " + pontos
+    }
+}
